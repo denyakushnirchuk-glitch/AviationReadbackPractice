7130d1addffccf75245f02b814a7f72a5d809191
DenyaK <denyakushnirchuk@gmail.com>
2025-11-15 16:43:35 +0100

    Finished the projects backbone, every function works properly incuding the user creation

--- README.md
+++ README.md
@@ -2,4 +2,19 @@
 Just a school project where I had to make an aviation readback practice app. Nothing too big out of it though
 
 
-just came to a new system
+**INSTALLATION INSTRUCTIONS**
+
+to get the script running on your computer, you need to use git. Using git, clone my repo to your device. Enjoy!
+
+
+
+**Use guidelines**
+
+For 100% working rate, please use the given username below:
+
+
+Username: admin
+password: 1234
+
+
+*Create your own user at your own risk haha, the admin is 1000 going to work.*
--- scripts/index.py
+++ scripts/index.py
@@ -1,5 +1,6 @@
 import time
 import json
+import random
 
 #importing questions
 from questions import questions
@@ -13,6 +14,13 @@ userChoice = ""
 quitPractice = False
 amountQuestions = 1
 currentquestion = 1
+
+#inside of practicce
+questionsCorrect = 1
+questionsWrong = 1
+questionNum = 3
+
+
 
 with open("database/userdatabase.json", "r") as file:
     database = json.load(file)
@@ -50,7 +58,7 @@ def appendDatabase():
       print(f"{x}. {i}")
       x += 1
 
-    selUser = input("Username: ")
+    selUser = input("Username: ").strip()
 
     if selUser in database["People"]:
         password = str(input("Password: "))
@@ -72,36 +80,109 @@ def appendDatabase():
 #INSIDE OF THE HUB--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 def menu():
-    time.sleep(2)
     skipLinez(10)
     print("Welcome to the hub!")
     print("1. Start Practice")
     print("2. Source the Code")
-    print("3. Exit")
+    print("3. View Stats")
+    print("4. Exit")
+
+
+
 
 def questionGiver():
+    global questionsCorrect
+    global questionsWrong
     global currentquestion
-    global amountQuestions
+    global questionNum
+    skipLinez(2)
+    time.sleep(1)
+    print(f"Question {currentquestion} / {amountQuestions}")
+    print(questions[questionNum]["questionDesc"])
+    print(questions[questionNum]["QuestionOPT"])
+    usrInput = input("Answer: ").strip().upper()
 
-    currentquestion = amountQuestions - amountQuestions + 1
+    if usrInput == questions[questionNum]["correctAns"]:
+        print("Answer correct!")
+        questionsCorrect += 1
+        currentquestion += 1
+    else:
+        print("Answer incorrect!!")
+        questionsWrong += 1
 
-    print(f"Question {currentquestion} of {amountQuestions}")
+
 
 def menuLogic():
     global userChoice
     global quitPractice
     global amountQuestions
+    global questionsCorrect
+    global questionsWrong
+    global currentquestion
+    global database
+    global questionNum
     skipLinez(1)
-    userChoice = input(":")
+    userChoice = int(input(":"))
     #setting the questions enviromentS
-    if input == "1":
+    if userChoice == 1:
         skipLinez(5)
-        amountQuestions = input("How many questions would you like (1 - 20): ")
+        amountQuestions = int(input("How many questions would you like (1 - 20): "))
         print("Starting practice. Get ready")
         for i in range(0, 3):
             print(".")
             time.sleep(1)
         skipLinez(5)
+        questionsCorrect = 0
+        questionsWrong = 0
+        currentquestion = 1
+
+        questionNum
+        for i in range (0, amountQuestions):
+            questionNum = random.randint(1, 20)
+            questionGiver()
+        print("Thats it!")
+        print(f"Correct: {questionsCorrect}")
+        print(f"Wrong: {questionsWrong}")
+
+        percentageRight = (questionsCorrect / amountQuestions) * 100
+        print(f"Your final score is: {percentageRight:.1f}%")
+
+
+        database["People"][loggedinas]["stats"]["ExamsCompleted"] += 1
+        if percentageRight >= 74:
+            print("You passed")
+            database["People"][loggedinas]["stats"]["CorrectStreak"] += 1
+            database["People"][loggedinas]["stats"]["ExamsCorrect"] += 1
+            
+        else:
+            print("You've failed")
+            database["People"][loggedinas]["stats"]["CorrectStreak"] = 0
+            database["People"][loggedinas]["stats"]["ExamsFailed"] += 1
+            
+        
+        with open("database/userdatabase.json", "w") as f:
+            json.dump(database, f, indent=2)
+        pass
+    elif userChoice == 2:
+        print("https://github.com/denyakushnirchuk-glitch/AviationReadbackPractice")
+        pass
+    elif userChoice == 3:
+        skipLinez(5)
+        print(f"Printing {loggedinas}'s stats")
+        print(f"Tests passed: {database["People"][loggedinas]["stats"]["ExamsCorrect"]}")
+        print(f"Tests failed: {database["People"][loggedinas]["stats"]["ExamsFailed"]}")
+        print(f"Correct streak: {database["People"][loggedinas]["stats"]["CorrectStreak"]}")
+        pass
+    elif userChoice == 4:
+        quitPractice = True
+        print("See you later!!")
+        pass
+    else:
+        print("Please enter a valid choice!")
+
+
+
+
 
 
 
@@ -111,10 +192,11 @@ def menuLogic():
 while not loggedin:
     appendDatabase()
 
-menu()
-menuLogic()
 
-time.sleep(2)
+while not quitPractice:
+    menu()
+    menuLogic()
+
 #print(loggedinas) #debug function, keep commented unless you know what you are doing
 
 
